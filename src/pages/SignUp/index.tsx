b68c22535f5ea1b26c4b194d8f90a660e1df6c08
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-07-13 23:20:59 -0300

    Implement form validation with Yup

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,19 +1,32 @@
-import React from 'react';
-
+import React, { useCallback } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
-
 import { Form } from '@unform/web';
-
+import * as Yup from 'yup';
 import { Container, Content, Background } from './styles';
+
 import logoImg from '../../assets/logo.svg';
 
 import Input from '../../components/Input/index';
 import Button from '../../components/Button/index';
 
 const SignUp: React.FC = () => {
-  function handleSubmit(data: any): void {
-    console.log(data);
-  }
+  const handleSubmit = useCallback(async (data: any) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string()
+          .required('Email obrigatório')
+          .email('Digite um email válido'),
+        password: Yup.string().min(6, 'Mínimo de 6 dígitos'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
 
   return (
     <>
