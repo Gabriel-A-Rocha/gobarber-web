85ec4b6f779c6a357301968c2385f1d1ff575627
Gabriel-A-Rocha <gabriel.altair.rocha@gmail.com>
2020-08-22 22:57:46 -0300

    Create scheduled appointments page section

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import { FiPower, FiClock } from 'react-icons/fi';
+import {
+  Container,
+  Header,
+  HeaderContent,
+  Profile,
+  Content,
+  Schedule,
+  Calendar,
+  NextAppointment,
+} from './styles';
 
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -29,6 +38,35 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Scheduled appointments</h1>
+          <p>
+            <span>Today</span>
+            <span>August 22</span>
+            <span>Saturday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Next appointments</strong>
+            <div>
+              <img
+                src="https://avatars2.githubusercontent.com/u/60102062?s=460&u=d46cf65e8d14695ff7ef324c1bc2d903d8248f24&v=4"
+                alt="Gabriel A Rocha"
+              />
+
+              <strong>Gabriel A. Rocha</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar />
+      </Content>
     </Container>
   );
 };
